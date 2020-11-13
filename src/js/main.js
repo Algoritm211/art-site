ec9aa06e8fc18c4e537cf2c81782dca7f762da40
Algoritm211 <60394886+Algoritm211@users.noreply.github.com>
2020-11-13 02:03:57 +0200

    Add DragAndDrop module

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import accordion from './modules/accordion'
 import burger from './modules/burger'
 import calc from './modules/calc'
 import checkTextInputs from './modules/checkTextInputs'
+import drop from './modules/drop'
 import filter from './modules/filter'
 import forms from './modules/forms'
 import mask from './modules/mask'
@@ -27,4 +28,5 @@ window.addEventListener('DOMContentLoaded', () => {
   accordion('.accordion-heading')
   burger('.burger-menu', '.burger')
   scrolling('.pageup')
+  drop()
 })
